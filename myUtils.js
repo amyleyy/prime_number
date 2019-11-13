aa86e6eac03db182b0a27b2af6f64a9a24e13dc6
amyleyy <57391602+amyleyy@users.noreply.github.com>
2019-11-13 22:45:42 +0400

    Update myUtils.js

--- myUtils.js
+++ myUtils.js
@@ -1,19 +1,34 @@
+const reader = require('readline-sync');
+let firstNumber = reader.question("Please insert a positive integer: ");
+let secondNumber = reader.question("Please inserrt another positive integer: ");
+
+let x = parseInt(firstNumber);
+let y = parseInt(secondNumber);
+
+let array = new array;
+for(let i = x; i <= y; i++) {
+    if(isPrime(i)) {
+        array.push(i);
+    }
+}
+
+
 module.exports = {
-	isPrime: function(n) {
-		if(n <=1) {
-			return false;
-		}
+    isPrime: function(n) {
+        if(n <=1) {
+            return false;
+        }
 
-		let div = 2;
+        let div = 2;
 
-		while(div <= Math.sqrt(n)) {
-			if(n % div === 0) {
-				return false;
-			}
+        while(div <= Math.sqrt(n)) {
+            if(n % div === 0) {
+                return false;
+            }
 
-			div++;
-		}
+            div++;
+        }
 
-		return true;
-	}
+        return true;
+    }
 }
